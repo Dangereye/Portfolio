287d2a650826dbf51a4c54b4d37583c2530d9ca8
Craig Puxty <craig_puxty@talktalk.net>
2021-06-08 21:54:44 +0100

    Added form functionality, with Netlify

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,10 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import SectionTitle from "../shared/SectionTitle";
 gsap.registerPlugin(ScrollTrigger);
 
 const Contact = () => {
+  const [company, setCompany] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [all, setAll] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setAll({ company, firstName, lastName, email, subject, message });
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encodeURI({
+        "form-name": "Contact",
+        ...all,
+      }),
+    })
+      .then(() => {
+        alert("Success!");
+      })
+      .catch((error) => alert(error));
+  };
   useEffect(() => {
     gsap.from(".line", {
       y: 100,
@@ -23,32 +47,67 @@ const Contact = () => {
     <section id="contact">
       <div className="container">
         <SectionTitle sub="Let's talk" title="Contact Me" />
-        <form name="contact" method="POST" data-netlify="true" className="form">
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          className="form"
+          onSubmit={handleSubmit}
+        >
           <div className="form__group line">
             <label htmlFor="company">Company</label>
-            <input type="text" id="company" name="company" />
+            <input
+              type="text"
+              id="company"
+              name="company"
+              onChange={(e) => setCompany(e.target.value)}
+            />
           </div>
           <div className="form__groups line">
             <div className="form__group">
               <label htmlFor="first-name">First Name</label>
-              <input type="text" id="first-name" name="first-name" />
+              <input
+                type="text"
+                id="first-name"
+                name="first-name"
+                onChange={(e) => setFirstName(e.target.value)}
+              />
             </div>
             <div className="form__group">
               <label htmlFor="last-name">Last Name</label>
-              <input type="text" id="last-name" name="last-name" />
+              <input
+                type="text"
+                id="last-name"
+                name="last-name"
+                onChange={(e) => setLastName(e.target.value)}
+              />
             </div>
           </div>
           <div className="form__group line">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="form__group line">
             <label htmlFor="subject">Subject</label>
-            <input type="text" id="subject" name="subject" />
+            <input
+              type="text"
+              id="subject"
+              name="subject"
+              onChange={(e) => setSubject(e.target.value)}
+            />
           </div>
           <div className="form__group line">
             <label htmlFor="message">Message</label>
-            <textarea id="message" name="message"></textarea>
+            <textarea
+              id="message"
+              name="message"
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
           </div>
           <button type="submit" className="btn primary large">
             Submit
