dd80778157ce4616a60bd212d0ec61f354ef9055
Craig Puxty <craig_puxty@talktalk.net>
2021-06-13 18:36:37 +0100

    Updated Mobile navigation

--- src/components/layout/MobileNav.js
+++ src/components/layout/MobileNav.js
@@ -1,47 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { IoHome } from "react-icons/io5";
-import { IoMdPerson, IoMdBrush } from "react-icons/io";
-import { HiDocumentDownload } from "react-icons/hi";
-import { FaComments, FaBook } from "react-icons/fa";
 
 const MobileNav = ({ navIsOpen, setNavIsOpen }) => {
   return (
     <nav className={navIsOpen ? "mobile-nav open" : "mobile-nav"}>
+      <h4>Navigation </h4>
       <ul className="mobile-nav__links">
         <li className="mobile-nav__link">
-          <Link to="/" onClick={() => setNavIsOpen(false)}>
-            <IoHome className="mobile-nav__icon" />
-            <span className="mobile-nav__text">Home</span>
+          <Link to="/#portfolio" onClick={() => setNavIsOpen(false)}>
+            Portfolio
           </Link>
         </li>
         <li className="mobile-nav__link">
           <Link to="/#skills" onClick={() => setNavIsOpen(false)}>
-            <FaBook className="mobile-nav__icon" />
-            <span className="mobile-nav__text">Skills</span>
+            Skills
           </Link>
         </li>
-        <li className="mobile-nav__link">
-          <Link to="/#portfolio" onClick={() => setNavIsOpen(false)}>
-            <IoMdBrush className="mobile-nav__icon" />
-            <span className="mobile-nav__text">Portfolio</span>
-          </Link>
-        </li>
+
         <li className="mobile-nav__link">
           <Link to="/#about" onClick={() => setNavIsOpen(false)}>
-            <IoMdPerson className="mobile-nav__icon" />
-            <span className="mobile-nav__text">About</span>
+            About
           </Link>
-        </li>
-        <li className="mobile-nav__link">
-          <HiDocumentDownload className="mobile-nav__icon" />
-          <span className="mobile-nav__text">Download CV</span>
         </li>
         <li className="mobile-nav__link">
           <Link to="/#contact" onClick={() => setNavIsOpen(false)}>
-            <FaComments className="mobile-nav__icon" />
-            <span className="mobile-nav__text">Contact</span>
+            Contact
           </Link>
+        </li>
+      </ul>
+      <h4>Downloads</h4>
+      <ul className="mobile-nav__links">
+        <li className="mobile-nav__link">
+          <a href="/cv/cv.txt" download>
+            Curriculum vitae
+          </a>
+        </li>
+      </ul>
+      <h4>Email</h4>
+      <ul className="mobile-nav__links">
+        <li className="mobile-nav__link">
+          <a href="mailto:craig_puxty@talktalk.net">craig_puxty@talktalk.net</a>
         </li>
       </ul>
     </nav>
