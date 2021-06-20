87adcb1a2b2d9749d83612e4c380b28ae927538b
Craig Puxty <craig_puxty@talktalk.net>
2021-06-20 10:09:11 +0100

    Updated Workwear Carousel section

--- src/components/sections/workwear/Carousel.js
+++ src/components/sections/workwear/Carousel.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const Carousel = ({ project, openSlideshow }) => {
+  useEffect(() => {
+    const carousel = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#carousel",
+        start: "top 300",
+        toggleActions: "play none none none",
+        markers: false,
+      },
+    });
+    carousel.from(".carousel-item, .slide", {
+      y: 100,
+      opacity: 0,
+      duration: 1,
+      stagger: 0.1,
+      ease: "back.out(2.5)",
+    });
+  }, []);
   return (
-    <section id="carousel" className="workwear">
+    <section id="carousel">
       <div className="container">
-        <h3 className="subtitle">Carousel images</h3>
-        <p>
+        <div className="section-subtitle carousel-item">Carousel</div>
+        <h2 className="section-title carousel-item">Hero Images</h2>
+        <p className="section-description carousel-item">
           Each image contains multiple layers, composited using numerous learned
           techniques to blend layers onto newly selected/created backgrounds.
           Click any image to view as a slideshow.
